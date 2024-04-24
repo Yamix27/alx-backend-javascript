eca826a064861edcbf5330403e92ad8a5b3484e3
Yamix27 <amine.boutallaght@gmail.com>
2024-04-24 14:03:03 +0000

    Done

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+iexport default function iterateThroughObject(reportWithIterator) {
+  const returnString = [];
+  for (const report of reportWithIterator) {
+    returnString.push(report);
+  }
+  return returnString.join(' | ');
+}
